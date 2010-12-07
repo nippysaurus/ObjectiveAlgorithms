e7945096591b112935c0d77295e195456020af9f
Michael Dawson <nippysaurus@me.com>
2010-12-07 11:20:04 +1000

    added mandlebrot algorithm placeholder

---
+++ NIPMandlebrot.h
@@ -0,0 +1,27 @@
+//
+//  NIPMandlebrot.h
+//  ObjectiveAlgorithms
+//
+//  Created by Michael Dawson on 7/12/10.
+//  Copyright 2010 Nippysaurus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//
+// NOTE: This file is just a placeholder for the moment. I do not know how
+// mandlebrot algorithms work and am not sure if this will ever work, but
+// it is my intention to make something useful. This file is just a placeholder
+// for the moment.
+//
+
+@interface NIPMandlebrot : NSObject {
+
+}
+
+//+(NSArray*)shuffleData:(NSArray*)data;
+//+(NSArray*)shuffleData:(NSArray*)data;
+
++(void)test;
+
+@end
---
+++ NIPMandlebrot.m
@@ -0,0 +1,13 @@
+//
+//  NIPMandlebrot.m
+//  ObjectiveAlgorithms
+//
+//  Created by Michael Dawson on 7/12/10.
+//  Copyright 2010 Nippysaurus. All rights reserved.
+//
+
+#import "NIPMandlebrot.h"
+
+@implementation NIPMandlebrot
+
+@end
