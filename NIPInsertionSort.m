ae7809f6976189de687e382201d0ad2622a96e6b
Michael Dawson <nippysaurus@me.com>
2010-12-15 10:46:06 +1000

    added very rough code implementation of insertion sort. I wrote the code in a text editor and will certainly not compile yet.

--- NIPInsertionSort.m
+++ NIPInsertionSort.m
@@ -12,7 +12,42 @@
 
 +(NSArray*)sortItems:(NSArray*)items usingComparator:(NSComparator)comparator
 {
-	//
+   // { This procedure sorts in ascending order. }
+   //     for i := 1 to length[A]-1 do
+   foreach (int i = 1 ; i < [items count] ; ++i)
+   {
+      id value = [items objectAtIndex: i];
+   //         value := A[i];
+      id j = i - 1;
+   //         j := i - 1;
+      bool done = NO;
+   //         done := false;
+      while (done == NO)
+      {
+   //         repeat
+   //             { To sort in descending order simply reverse the operator i.e. A[j] < value }
+         id compareTo = [items objectAtIndex:j];
+         if (comparator(compareTo, value) == NSBigger)
+         {
+   //             if A[j] > value then
+   //             begin
+   //                 A[j + 1] := A[j];
+   //                 j := j - 1;
+   //                 if j < 0 then
+   //                     done := true;
+   //             end
+         }
+         else
+         {
+            done = YES;
+         }
+   //             else
+   //                 done := true;
+   //         until done;
+      }
+      [items setObject:value forIndex:j+1];
+   //         A[j + 1] := value;
+   }
 }
 
 +(void)test
